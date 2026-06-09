78c1e8450e0afda77ed6f7f7c064071d866fe96c
JCPrime01 <vanzokiller@gmail.com>
2026-06-08 23:07:30 -0300

    chore: shareCode 5D1WWZ0MMOB -> IF1L0D35UIR (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6e81a4di?shareCode=5D1WWZ0MMOB&afp=ti' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6e81a4di?shareCode=IF1L0D35UIR&afp=ti' });
     res.end();
   }
